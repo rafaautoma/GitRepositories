6f97293a312d294504051f2db351fe107cfa1ceb
Rafael <Automao@DESKTOP-0SR594A.(none)>
2023-03-29 08:23:05 -0300

    Projeto

--- Projeto.py
+++ Projeto.py
@@ -1,1 +1,32 @@
+# Study project VS Code
+# Description: Project to learn how to use the VS Code code editor
+# Author: Rafael Kruger Schwertz
+# Creation date: 2023-03-29
+# Last update: 2023-03-29
+# Version: 1.0
+# Platform: Windows 10
+# Programming language: Python
+# Libraries used: N/A
+# Additional Notes: N/A
+
+# Importações necessárias:
+import os
 import pandas as pd
+
+# Definição das funções:
+
+def ler_arquivo(nome_arquivo):
+    # Lê o arquivo especificado e retorna um dataframe com seus dados
+    pass
+
+def processar_dados(dataframe):
+    # Processa os dados e retorna um novo dataframe
+    pass
+
+# Código principal:
+
+if __name__ == "__main__":
+    nome_arquivo = "dados.csv"
+    dataframe = ler_arquivo(nome_arquivo)
+    novo_dataframe = processar_dados(dataframe)
+    # Faz alguma coisa com o novo dataframe
